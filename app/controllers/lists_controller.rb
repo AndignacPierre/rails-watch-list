c24af25bf7b14282b9cbd43776dfe763bde2461b
Pierre Andignac <pierreandignac.work@gmail.com>
2024-08-09 17:45:43 +0200

    ok

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,6 +21,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :title)
+    params.require(:list).permit(:name, :title, :photo)
   end
 end
